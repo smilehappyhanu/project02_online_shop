c0f9ea98ae1013f8fe7d6f8b39575618422519c9
TienHT <hoangtienhanu881996@gmail.com>
2025-03-15 15:07:36 +0700

    Update function authen admin

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
-class Authenticate extends Middleware
+class AdminAuthenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
@@ -15,7 +15,14 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+            return route('admin.login');
         }
+    }
+    protected function authenticate($request, array $guards)
+    {
+            if ($this->auth->guard('admin')->check()) {
+                return $this->auth->shouldUse('admin');
+            }
+        $this->unauthenticated($request, ['admin']);
     }
 }
