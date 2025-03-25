f77830e64d0c9ca16b2359887313d5768683660d
TienHT <hoangtienhanu881996@gmail.com>
2025-03-25 08:53:21 +0700

    Update function update and delete category

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -60,6 +60,76 @@ class CategoryController extends Controller
                 
 
             }
+            $request->session()->flash('success','Category updated successfully.');
+            return response()->json([
+                'status' => true,
+                'message' => 'Category updated successfully.'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+    }
+    public function edit($categoryId, Request $request) {
+        $category = Category::find($categoryId);
+        if(empty($category)) {
+            return redirect()->route('categories.index');
+        }
+        return view('admin.category.edit',compact('category'));
+    }
+
+    public function update($categoryId, Request $request) {
+        $category = Category::find($categoryId);
+
+        if(empty($category)) {
+            return response()->json([
+                'status' => false,
+                'notFound' => true,
+                'message' => 'Category not found.'
+            ]);
+        }
+        $validator = Validator::make($request->all(),[
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,'.$category->id.',id',
+        ]);
+        if($validator->passes()) {
+            $category = $this->category->find($categoryId)->update([
+                'name' => $request->name,
+                'slug' => $request->slug,
+                'status' => $request->status
+            ]);
+            $category = $this->category->find($categoryId);
+            $oldImage = $category->image;
+
+            // save image here
+            if(!empty($request->image_id)) {
+                $tempImage = TempImage::find($request->image_id);
+                $extArray = explode('.',$tempImage->name);
+                $ext = last($extArray);
+                $newImageName = $category->id.'-'. time().'.'.$ext;
+                $sPath = public_path()."/temp/".$tempImage->name;
+                $dPath = public_path().'/uploads/category/'.$newImageName;
+                File::copy($sPath,$dPath);
+
+                // Generate image thumbnail
+                $dPath = public_path().'/uploads/category/thumb/'.$newImageName;
+                $img = Image::make($sPath);
+                // $img->resize(450,600);
+                $img->fit(450,600,function($constraint){
+                    $constraint->upsize();
+                });
+                $img->save($dPath);
+                $category->image = $newImageName;
+                $category->save();
+                
+                // Delete old image here
+                File::delete(public_path().'/uploads/category/thumb/'.$oldImage);
+                File::delete(public_path().'/uploads/category/'.$oldImage);
+
+
+            }
             $request->session()->flash('success','Category added successfully.');
             return response()->json([
                 'status' => true,
@@ -72,13 +142,19 @@ class CategoryController extends Controller
             ]);
         }
     }
-    public function edit() {
-        
-    }
-    public function update() {
-        
-    }
-    public function destroy() {
-        
+    public function destroy($categoryId, Request $request) {
+        $category = Category::find($categoryId);
+        if(empty($category)) {
+            $request->session()->flash('error','Category not found');
+            return redirect()->route('categories.index');
+        }
+        File::delete(public_path().'/uploads/category/thumb/'.$category->image);
+        File::delete(public_path().'/uploads/category/'.$category->image);
+        $category->delete();
+        $request->session()->flash('success','Category deleted successfully');
+        return response()->json([
+            'status' => true,
+            'message' => 'Category deleted successfully'
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,11 @@ Route::group(['prefix' => 'admin'],function(){
         Route::get('/categories',[CategoryController::class,'index'])->name('categories.index');
         Route::get('/categories/create',[CategoryController::class,'create'])->name('categories.create');
         Route::post('/categories/store',[CategoryController::class,'store'])->name('categories.store');
+        Route::get('/categories/{category}/edit',[CategoryController::class,'edit'])->name('categories.edit');
+        Route::put('/categories/{category}',[CategoryController::class,'update'])->name('categories.update');
+        Route::delete('/categories/{category}',[CategoryController::class,'destroy'])->name('categories.delete');
+
+        
         Route::post('/upload-temp-image',[TempImagesController::class,'create'])->name('temp-images.create');
 
         Route::get('/getSlug',function(Request $request){
