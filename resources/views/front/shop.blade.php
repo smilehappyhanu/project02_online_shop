085f3b770ab8fe5e9628c72358764e2508b7965d
TienHT <hoangtienhanu881996@gmail.com>
2025-04-14 15:05:33 +0700

    Fix bug

--- resources/views/front/shop.blade.php
+++ resources/views/front/shop.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="light-font">
                 <ol class="breadcrumb primary-color mb-0">
-                    <li class="breadcrumb-item"><a class="white-text" href="{{route('front.home')">Home</a></li>
+                    <li class="breadcrumb-item"><a class="white-text" href="{{ route('front.home') }}">Home</a></li>
                     <li class="breadcrumb-item active">Shop</li>
                 </ol>
             </div>
